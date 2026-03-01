c9c1d034304c84b5341a6f873b3ffcb603110fb9
celuu <christine.e.luu@gmail.com>
2026-03-01 14:37:30 -0800

    Update controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,42 @@
 class PostsController < ApplicationController
 
   def index
-    @post = Post.all
+    @posts = Post.all
 
   end
 
   def show
-    @posts = Post.all
+    @post = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :index, status: :unprocessable_entity
+    end
+
+  end
+
+  def update
+    @post = Post.find(id: params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(id: params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post deleted"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+  
 end
